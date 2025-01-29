8cfcca1034dfcde42cdf43c90c255ce0dc513ccb
Sachin T <sachintu47@gmail.com>
2025-01-29 04:49:50 -0500

    Initial working commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/spdlogport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/spdlogport/actions/workflows/bump.yml)
+
+# spdlog
+
+Fast C++ logging library
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/spdlogport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/spdlogport.git'), string(name: 'PORT_DESCRIPTION', value: 'Fast C++ logging library' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
